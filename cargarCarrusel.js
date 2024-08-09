0db7ce13b500675f05c0ce0bda23ddd4031f4206
Cristian Fernández <129860008+Cristian-000@users.noreply.github.com>
2024-08-09 18:09:03 -0300

    Update cargarCarrusel.js

--- cargarCarrusel.js
+++ cargarCarrusel.js
@@ -1,28 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const currentPage = window.location.pathname.split('/').pop(); // Obtiene el nombre de la página actual
+    const categoriaSeleccionada = localStorage.getItem('categoriaSeleccionada');
 
-    fetch('categorias.json')
-        .then(response => response.json())
-        .then(data => {
-            let categoriaActual = null;
-            data.categorias.forEach(categoria => {
-                const fraseEncontrada = categoria.frases.find(frase => frase.enlace === currentPage);
-                if (fraseEncontrada) {
-                    categoriaActual = categoria;
+    if (categoriaSeleccionada) {
+        fetch('categorias.json')
+            .then(response => response.json())
+            .then(data => {
+                const categoria = data.categorias.find(cat => cat.nombre === categoriaSeleccionada);
+
+                if (categoria) {
+                    const carousel = document.querySelector('.carousel');
+                    categoria.frases.forEach(frase => {
+                        const link = document.createElement('a');
+                        link.href = frase.enlace;
+                        link.textContent = frase.titulo;
+                        carousel.appendChild(link);
+                    });
+                } else {
+                    console.error('Categoría no encontrada.');
                 }
-            });
-
-            if (categoriaActual) {
-                const carousel = document.querySelector('.carousel');
-                categoriaActual.frases.forEach(frase => {
-                    const link = document.createElement('a');
-                    link.href = frase.enlace;
-                    link.textContent = frase.titulo;
-                    carousel.appendChild(link);
-                });
-            } else {
-                console.error('Categoría no encontrada para la página actual.');
-            }
-        })
-        .catch(error => console.error('Error al cargar el JSON:', error));
+            })
+            .catch(error => console.error('Error al cargar el JSON:', error));
+    } else {
+        console.error('No se ha seleccionado ninguna categoría.');
+    }
 });
