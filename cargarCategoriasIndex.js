0ddb56c363212c5f140efef3286cf8f05421d8bd
Cristian Fernández <129860008+Cristian-000@users.noreply.github.com>
2024-08-09 20:06:30 -0300

    Create cargarCategoriasIndex.js

---
+++ cargarCategoriasIndex.js
@@ -0,0 +1,25 @@
+// Función para cargar el JSON de categorías en index
+function cargarListaIndex() {
+    fetch('categorias.json')
+        .then(response => response.json())
+        .then(data => {
+            const lista = document.getElementById('index-categorias');
+            lista.innerHTML = `
+                <h5>Categorías</h5>
+                <ul class="list-group">
+                    ${data.categorias.map(categoria => 
+                        `<li class="list-group-item"><a href="${categoria.enlace}" onclick="guardarCategoria('${categoria.nombre}')">${categoria.nombre}</a></li>`
+                    ).join('')}
+                </ul>
+            `;
+        })
+        .catch(error => console.error('Error al cargar las categorías para el aside:', error));
+}
+
+// Función para guardar la categoría seleccionada en localStorage
+function guardarCategoria(nombreCategoria) {
+    localStorage.setItem('categoriaSeleccionada', nombreCategoria);
+}
+
+// Cargar el aside al cargar la página
+document.addEventListener('DOMContentLoaded', cargarListaIndex);
