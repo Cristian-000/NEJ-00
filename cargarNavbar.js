24db6f8e8363a736d3ff77d27305a145a1ff75e6
Cristian Fernández <129860008+Cristian-000@users.noreply.github.com>
2024-08-10 10:36:23 -0300

    Update cargarNavbar.js

--- cargarNavbar.js
+++ cargarNavbar.js
@@ -6,27 +6,27 @@ function cargarNavbar() {
             const navbar = document.getElementById('navbar');
             navbar.innerHTML = `
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="index.html">No es Justo</a>
+                    <a class="navbar-brand fw-bold" href="index.html" style="color: var(--color-acento);">No es Justo</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav ms-auto">
                             <li class="nav-item">
-                                <a class="nav-link" href="index.html">Inicio</a>
+                                <a class="nav-link" href="index.html" style="color: var(--color-texto);">Inicio</a>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: var(--color-texto);">
                                     Categorías
                                 </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown" style="background-color: var(--color-secundario);">
                                     ${data.categorias.map(categoria => 
-                                        `<li><a class="dropdown-item" href="${categoria.enlace}" onclick="guardarCategoria('${categoria.nombre}')">${categoria.nombre}</a></li>`
+                                        `<li><a class="dropdown-item" href="${categoria.enlace}" onclick="guardarCategoria('${categoria.nombre}')" style="color: var(--color-texto);">${categoria.nombre}</a></li>`
                                     ).join('')}
                                 </ul>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="contacto.html">Contacto</a>
+                                <a class="nav-link" href="contacto.html" style="color: var(--color-texto);">Contacto</a>
                             </li>
                         </ul>
                     </div>
