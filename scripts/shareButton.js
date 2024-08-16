4df63c65a62234fc4474b6dbf51bc1dec966f7f4
Cristian Fernández <129860008+Cristian-000@users.noreply.github.com>
2024-08-16 19:03:23 -0300

    Update shareButton.js

--- scripts/shareButton.js
+++ scripts/shareButton.js
@@ -1,7 +1,7 @@
 const shareData = {
     title: 'Título de la Imagen',
     text: 'Descripción de la Imagen',
-    url: 'https://cristian-000.github.io/NEJ-00/pages/frases/amor_no_correspondido/amor_no_correspondido_1.html/imagen.jpg'
+    url: 'https://cristian-000.github.io/NEJ-00/Screenshot_20240816_174137_YouTube.jpg'
 };
 
 document.getElementById('share-button').addEventListener('click', async () => {
